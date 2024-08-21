7beb74ffc8e6151adcd442a43fdbfe4041d599e5
Sirvan Monfared <sirvan.monfared.dev@gmail.com>
2024-08-21 20:18:54 +0330

    s04e05

--- 4-crypto-bot/index.php
+++ 4-crypto-bot/index.php
@@ -40,8 +40,18 @@ if ($text === '/home') {
     $cryptoList = array_slice($result, 0, 10);
 
     $buttons = [];
-    foreach ($cryptoList as $crypto) {
-        $buttons[] = [$telegram->buildInlineKeyboardButton($crypto->name, callback_data: "/crypto/{$crypto->key}")];
+
+    $buttons[] = [
+        $telegram->buildInlineKeyboardButton('دلار', callback_data: "/currency/dollar"),
+        $telegram->buildInlineKeyboardButton('سکه', callback_data: "/currency/coin")
+    ];
+
+
+    foreach (array_chunk($cryptoList, 2) as $cryptoGroup) {
+        $buttons[] = [
+            $telegram->buildInlineKeyboardButton($cryptoGroup[0]->name, callback_data: "/crypto/{$cryptoGroup[0]->key}"),
+            $telegram->buildInlineKeyboardButton($cryptoGroup[1]->name, callback_data: "/crypto/{$cryptoGroup[1]->key}")
+        ];
     }
 
     $keyboard = $telegram->buildInlineKeyBoard($buttons);
@@ -64,6 +74,51 @@ if ($text === '/home') {
     //     'chat_id' => $chat_id,
     //     'message_id' => $loading_message['result']['message_id']
     // ]);
+}
+
+if (str_starts_with($text, '/currency/')) {
+    $key = str_replace('/currency/', '', $text);
+
+    if ($key !== 'dollar' && $key !== 'coin') {
+        $telegram->sendMessage([
+            'chat_id' => $chat_id,
+            'text' => "دستور وارد شده معتبر نیست ❌",
+        ]);
+
+        return;
+    }
+
+    $loading_message = $telegram->sendMessage([
+        'chat_id' => $chat_id,
+        'text' => 'در حال دریافت اطلاعات ...'
+    ]);
+
+    $list = getCurrencyList();
+
+    if ($key === 'dollar') {
+        $text = "قیمت روز دلار: " . $list->currencies->dollar->p . "\n\n";
+        $text .= "بالاترین قیمت روز دلار: " . $list->currencies->dollar->h . "\n\n";
+        $text .= "پایین ترین قیمت روز دلار: " . $list->currencies->dollar->l . "\n\n";
+    }
+
+    if ($key === 'coin') {
+        $text = "قیمت روز سکه گرمی: " . $list->coin->gerami->p . "\n\n";
+        $text .= " قیمت روز سکه ربع: " . $list->coin->rob->p . "\n\n";
+        $text .= "  قیمت نیم سکه  " . $list->coin->nim->p . "\n\n";
+        $text .= "  قیمت سکه تمام  " . $list->coin->sekee->p . "\n\n";
+    }
+
+
+    $keyboard = $telegram->buildInlineKeyBoard([
+        [$telegram->buildInlineKeyboardButton('بازگشت', callback_data: "/home")]
+    ]);
+
+    $telegram->editMessageText([
+        'chat_id' => $chat_id,
+        'reply_markup' => $keyboard,
+        'message_id' => $loading_message['result']['message_id'],
+        'text' => $text,
+    ]);
 }
 
 if (str_starts_with($text, '/crypto/')) {
