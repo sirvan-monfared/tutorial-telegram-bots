a720648af01be0bf7efa08b84d5b05ac85827112
Sirvan Monfared <sirvan.monfared.dev@gmail.com>
2024-08-21 19:39:17 +0330

    s04e04

---
+++ 4-crypto-bot/functions.php
@@ -0,0 +1,23 @@
+<?php
+
+function getCryptoList()
+{
+    $curl = curl_init();
+
+    curl_setopt_array($curl, array(
+        CURLOPT_URL => 'https://one-api.ir/DigitalCurrency/?token=' . $_ENV['ONE_API_TOKEN'],
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => '',
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 0,
+        CURLOPT_FOLLOWLOCATION => true,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => 'GET',
+    ));
+
+    $response = curl_exec($curl);
+
+    curl_close($curl);
+
+    return (json_decode($response))->result;
+}
--- 4-crypto-bot/index.php
+++ 4-crypto-bot/index.php
@@ -11,4 +11,100 @@ $telegram = new Telegram($_ENV['TELEGRAM_BOT_TOKEN'], proxy: [
     'port' => '12334'
 ]);
 
-echo $telegram->ChatID();
+$text = $telegram->Text();
+$chat_id = $telegram->ChatId();
+$data = $telegram->getData();
+
+if ($text === '/start') {
+
+    $keyboard = $telegram->buildInlineKeyBoard([
+        [$telegram->buildInlineKeyboardButton('مشاهده لیست ارزها', callback_data: "/home")]
+    ]);
+
+    $telegram->sendMessage([
+        'chat_id' => $chat_id,
+        'reply_markup' => $keyboard,
+        'text' => "برای شروع روی لینک زیر کلیک کنید",
+    ]);
+}
+
+if ($text === '/home') {
+
+    $loading_message = $telegram->sendMessage([
+        'chat_id' => $chat_id,
+        'text' => 'در حال دریافت اطلاعات ...'
+    ]);
+
+    $result = getCryptoList();
+
+    $cryptoList = array_slice($result, 0, 10);
+
+    $buttons = [];
+    foreach ($cryptoList as $crypto) {
+        $buttons[] = [$telegram->buildInlineKeyboardButton($crypto->name, callback_data: "/crypto/{$crypto->key}")];
+    }
+
+    $keyboard = $telegram->buildInlineKeyBoard($buttons);
+
+    $telegram->editMessageText([
+        'chat_id' => $chat_id,
+        'reply_markup' => $keyboard,
+        'text' => "لطفا روی یکی از گزینه های زیر کلیک کنید",
+        'message_id' => $loading_message['result']['message_id']
+    ]);
+
+    // $telegram->editMessageText([
+    //     'chat_id' => $chat_id,
+    //     'reply_markup' => $keyboard,
+    //     'text' => "لطفا روی یکی از گزینه های زیر کلیک کنید",
+    //     'message_id' => $telegram->MessageID(),
+    // ]);
+
+    // $telegram->deleteMessage([
+    //     'chat_id' => $chat_id,
+    //     'message_id' => $loading_message['result']['message_id']
+    // ]);
+}
+
+if (str_starts_with($text, '/crypto/')) {
+    $crypto_key = str_replace('/crypto/', '', $text);
+
+
+    $loading_message = $telegram->sendMessage([
+        'chat_id' => $chat_id,
+        'text' => 'در حال دریافت اطلاعات ...'
+    ]);
+
+    $list = getCryptoList();
+
+    $result = array_filter($list, fn($crypto) => $crypto->key === $crypto_key);
+    $crypto = array_shift($result);
+
+    $text = "نام: {$crypto->name} \n\n";
+    $text .= "نام اصلی: {$crypto->name_en} \n\n";
+    $text .= " قیمت: " . number_format($crypto->price) . " دلار \n\n";
+
+
+    $keyboard = $telegram->buildInlineKeyBoard([
+        [$telegram->buildInlineKeyboardButton('بازگشت', callback_data: "/home")]
+    ]);
+
+    $telegram->editMessageText([
+        'chat_id' => $chat_id,
+        'reply_markup' => $keyboard,
+        'message_id' => $loading_message['result']['message_id'],
+        'text' => $text,
+    ]);
+
+    // $telegram->editMessageText([
+    //     'chat_id' => $chat_id,
+    //     'reply_markup' => $keyboard,
+    //     'message_id' => $telegram->MessageID(),
+    //     'text' => $text,
+    // ]);
+
+    // $telegram->deleteMessage([
+    //     'chat_id' => $chat_id,
+    //     'message_id' => $loading_message['result']['message_id']
+    // ]);
+}
